7c960589336de015c8cba97bf24e24dfce946f4d
zdodev <iver0945@gmail.com>
2020-12-01 16:34:31 +0900

    feat: 화면 터치 시 키보드 사라지도록 구현

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -10,9 +10,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
