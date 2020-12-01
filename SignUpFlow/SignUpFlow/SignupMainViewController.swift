c1f271b93b6bcb013c6854cad9bda6cd7c0ceed7
SM <hcooch2ch3@gmail.com>
2020-12-02 02:37:49 +0900

    feat: Signup main화면 프로필 사진 선택 기능 구현

--- SignUpFlow/SignUpFlow/SignupMainViewController.swift
+++ SignUpFlow/SignUpFlow/SignupMainViewController.swift
@@ -9,8 +9,37 @@ import UIKit
 
 class SignupMainViewController: UIViewController {
 
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    private let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(presentImagePicker))
+        userImageView.addGestureRecognizer(tap)
+    }
+    
+    @objc func presentImagePicker() {
+        self.present(imagePicker, animated: true, completion: nil)
     }
 
+}
+
+extension SignupMainViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            print("이미지를 불러올 때 문제가 발생하였습니다.")
+            return
+        }
+        
+        userImageView.image = image
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
