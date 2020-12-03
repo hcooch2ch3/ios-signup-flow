55a4c1b1d9147eb5fdf996562cfe78655a531c34
zdodev <iver0945@gmail.com>
2020-12-03 16:51:26 +0900

    step4 sample 초기화

---
+++ SignUpFlow/SignUpFlow/SignUpStep2ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class SignUpStep2ViewController: UIViewController {
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UILabel!
+    @IBOutlet weak var birthdayDatePicker: UIDatePicker!
+    @IBOutlet weak var signinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pressedDatePicker(_ sender: UIDatePicker) {
+    }
+    
+    @IBAction func pressedCancelButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func pressedBackButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func pressedSigninButton(_ sender: UIButton) {
+    }
+}
